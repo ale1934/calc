edeffa0e58a444009fa13c09b2a96462e356600c
ale <alesees10@gmail.com>
2024-10-12 18:49:10 -0500

    new design

--- app.js
+++ app.js
@@ -1,49 +1,13 @@
-let num1 = "";
-let num2 = "";
-let oper;
-let result;
-let curNum = 1;
+const display = document.getElementById("display");
 
-function number(x) {
-  if (curNum == 1) {
-    num1 += x;
-    console.log(num1);
-    document.getElementById('num1').innerHTML = num1;
-
-  } else {
-    num2 += x;
-    console.log(num2);
-    document.getElementById('num2').innerHTML = num2;
-
-  }
+function appendToDisplay(input) {
+  display.value += input;
 }
 
-function operator(x) {
-  oper = x;
-  console.log(oper);
-  document.getElementById('oper').innerHTML = oper;
-  curNum = 2;
+function clearDisplay() {
+  display.value = "";
 }
 
-function refresh() {
-  location.reload()
-}
-
-function getResult() {
-  if (oper == "+")
-  {
-    result = Number(num1) + Number(num2);
-  } else if (oper == "-") {
-    result = Number(num1) - Number(num2);
-  } else if (oper == "*") {
-    result = Number(num1) * Number(num2);
-  } else if (oper == "/") {
-    result = Number(num1) / Number(num2);
-  } else {
-    console.log("error! no operator!")
-  }
-
-  console.log(result);
-  document.getElementById('result').innerHTML = result;
-  num1 = ""; num2 = "";
+function calculate() {
+  display.value = eval(display.value);
 }
