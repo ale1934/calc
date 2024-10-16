3f0f7993bfe5a41a027552fa5816cdc14a740b0e
ale <alesees10@gmail.com>
2024-10-16 14:00:18 -0500

    can now use keybaord!

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const display = document.getElementById("display");
+const listOfKeys = ['1', '2', '3', '4',
+                    '5', '6', '7', '8',
+                    '9', '0', '*', '-',
+                    '+', '%', '(', ')',
+                    '/', '.', 'Enter'];
 
 function appendToDisplay(input) {
   display.value += input;
@@ -11,3 +16,18 @@ function clearDisplay() {
 function calculate() {
   display.value = eval(display.value);
 }
+
+document.addEventListener('keydown', function(event) {
+  console.log('Key pressed:', event.key);
+  if (event.key == 'Backspace') {
+    display.value = display.value.slice(0, -1);
+  }
+  if (listOfKeys.includes(event.key)) {
+    if (event.key == 'Enter')
+    {
+      calculate();
+    } else {
+      display.value += event.key;
+    }
+  }
+});
